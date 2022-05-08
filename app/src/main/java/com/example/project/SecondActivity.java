6f790049d0ad60c4fca88aa144dce79f80a5dfc4
c21adano <c21adano@student.his.se>
2022-05-08 19:13:25 +0200

    Added new activity called SecondActivity/activity_second. added button to both activities. Added code to allow transition between both activities.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,34 +1,32 @@
 package com.example.project;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+public class SecondActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
     private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        button = (Button) findViewById(R.id.buttonOne);
+        button = (Button) findViewById(R.id.buttonTwo);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                openActivity1();
 
             }
         });
 
     }
-    public void openActivity2(){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void openActivity1(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
