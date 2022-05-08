80b1fa387cdf738f4ac56b518cb0cdf368772f0b
c21adano <c21adano@student.his.se>
2022-05-08 19:37:40 +0200

    Added preference code

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.project;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t1;
+        t1 = findViewById(R.id.textThatGotEditThing);
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String name = sp.getString("name", "");
+
+        t1.setText(name);
 
         button = (Button) findViewById(R.id.buttonOne);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,23 +2,42 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button button;
+
+    EditText name;
+    SharedPreferences sp;
+    String TextThing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        name = findViewById(R.id.TexteditThing);
         button = (Button) findViewById(R.id.buttonTwo);
+
+        sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TextThing = name.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.putString("text", TextThing);
+                editor.commit();
+
                 openActivity1();
 
             }
